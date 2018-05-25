88f198f91084b9980027d7f8e00df6d23060f752
Seva Zaikov <seva.zaikov@gmail.com>
2018-05-26 01:33:27 +0200

    move API keys check to the app.settings

--- app/settings.py
+++ app/settings.py
@@ -2,3 +2,14 @@ import os
 
 BING_KEY = os.environ.get('BING_KEY')
 DARKSKY_KEY = os.environ.get('DARKSKY_KEY')
+
+def check_keys():
+  if BING_KEY is None:
+    print("You have to provide your Bing Maps Key")
+    print("More info at https://www.bingmapsportal.com/")
+    exit(1)
+
+  if DARKSKY_KEY is None:
+    print("You have to provide your DarkSky Maps Key")
+    print("More info at https://darksky.net/dev")
+    exit(1)
--- run.py
+++ run.py
@@ -1,17 +1,10 @@
 import argparse
-from app.settings import BING_KEY, DARKSKY_KEY
+from app.settings import BING_KEY, DARKSKY_KEY, check_keys
 from app.geo import load_geocodes, choose_resource
 from app.forecast import load_forecast, print_current_weather
 
-if BING_KEY is None:
-  print("You have to provide your Bing Maps Key")
-  print("More info at https://www.bingmapsportal.com/")
-  exit(1)
-
-if DARKSKY_KEY is None:
-  print("You have to provide your DarkSky Maps Key")
-  print("More info at https://darksky.net/dev")
-  exit(1)
+# we can't proceed without API keys
+check_keys()
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--city", help="City to get weather")
@@ -36,7 +29,6 @@ def get_city_data():
   resources = geocodes['resourceSets'][0]['resources']
   city_object = choose_resource(resources)
   forecast = load_forecast(city_object)
-
   print_current_weather(forecast)
 
 if __name__ == "__main__":
