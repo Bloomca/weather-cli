fdf6e8cf70a6af77da75bd7804e49ee42c02b830
Seva Zaikov <seva.zaikov@gmail.com>
2018-05-26 01:53:04 +0200

    move all logic to the package

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from .settings import check_keys
+from .geo import load_geocodes
+from .input import get_city, choose_resource
+from .forecast import load_forecast, print_current_weather
+
+def get_city_data():
+  city = get_city()
+  geocodes = load_geocodes(city)
+  resources = geocodes['resourceSets'][0]['resources']
+  city_object = choose_resource(resources)
+  forecast = load_forecast(city_object)
+  print_current_weather(forecast)
--- run.py
+++ run.py
@@ -1,19 +1,9 @@
-import argparse
-from app.settings import BING_KEY, DARKSKY_KEY, check_keys
-from app.geo import load_geocodes
-from app.input import get_city, choose_resource
-from app.forecast import load_forecast, print_current_weather
+from app import get_city_data
+from app.settings import check_keys
 
-# we can't proceed without API keys
-check_keys()
-
-def get_city_data():
-  city = get_city()
-  geocodes = load_geocodes(city)
-  resources = geocodes['resourceSets'][0]['resources']
-  city_object = choose_resource(resources)
-  forecast = load_forecast(city_object)
-  print_current_weather(forecast)
 
 if __name__ == "__main__":
+  # we can't proceed without API keys
+  check_keys()
+
   get_city_data()
